d03ebdf37d808d02d20f1d74e9c9e4452f629f9a
Zurceel <gabrielcruzsil@gmail.com>
2022-10-19 15:30:36 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpDevagram/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using CSharpDevagram.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpDevagram.Controllers
+{
+
+	[ApiController]
+	[Route("api/[controller]")]
+	public class LoginController : ControllerBase
+	{
+
+		private readonly ILogger<LoginController> _logger;
+
+		public LoginController(ILogger<LoginController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost]
+		[AllowAnonymous]
+
+		public IActionResult EfetuarLogin([FromBody] LoginRequisicaoDto loginrequisicao)
+		{
+			try
+			{
+				throw new ArgumentException("Erro ao preencher dados");
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError("Ocorreu um erro ao fazer login: " + ex.Message);
+				return StatusCode(StatusCodes.Status500InternalServerError, new ErrorRespostaDto()
+				{
+					Descricao = "Ocorreu um erro ao fazer login",
+					Status = StatusCodes.Status500InternalServerError
+				});
+			}
+
+		}
+
+
+
+	}
+}
